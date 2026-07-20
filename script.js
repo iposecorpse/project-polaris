dbee2a83387b3eb62526baf0a938659e1198d7ac
iposecorpse <iposecorpse@gmail.com>
2026-07-20 12:34:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
+const button = document.getElementById("beginBtn");
+const letter = document.getElementById("letter");
+const typing = document.getElementById("typing");
 
+const message = `Dear Sia,
+
+Happy Birthday. ❤️
+
+Today is all about celebrating you.
+
+I hope this year brings you happiness,
+good health,
+lots of laughter,
+and memories worth keeping.
+
+Thank you for being the wonderful person you are.
+
+Have an amazing birthday.
+
+— Deepak`;
+
+button.addEventListener("click", () => {
+
+button.style.display = "none";
+
+letter.classList.remove("hidden");
+
+typeWriter();
+
+});
+
+let i = 0;
+
+function typeWriter(){
+
+if(i < message.length){
+
+typing.innerHTML += message.charAt(i);
+
+i++;
+
+setTimeout(typeWriter,35);
+
+}
+
+}
